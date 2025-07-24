2a9e0d2ddc6aaf7b03e1660d82aeae7549630665
panayiotayiannitsarou <panayiotaconstantinou818@gmail.com>
2025-07-24 20:32:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if uploaded_file:
     all_stats = []
 
     for i, scenario_df in enumerate(scenarios):
-        scenario_df = step2_zoiroi(scenario_df, num_classes)
+        scenario_df = step2_zoiroi(scenario_df, num_classes, senario_col)
         scenario_df = step3_idiaiterotites(scenario_df, num_classes)
         scenario_df = step4_amivaia_filia(scenario_df)
         scenario_df = step5_filikoi_omades(scenario_df, num_classes)
